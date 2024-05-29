3c8142a152a8e59ce0ff72f2182b537af17a5f01
DucAnh <anhnguyen2k373@gmail.com>
2024-05-29 08:54:07 +0700

    Add Project Test Mart

---
+++ sieu_thi_mini/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace sieu_thi_mini
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public class MaDangNhapService
+    {
+        private string _maDangNhap;
+
+        public string MaDangNhap
+        {
+            get { return _maDangNhap; }
+            set { _maDangNhap = value; }
+        }
+    }
+
+    public class MatKhauService
+    {
+        private string _matKhau;
+
+        public string MatKhau
+        {
+            get { return _matKhau; }
+            set { _matKhau = value; }
+        }
+    }
+    public partial class App : Application
+    {
+        public static MaDangNhapService MaDangNhapService { get; } = new MaDangNhapService();
+        public static MatKhauService MatKhauService { get; } = new MatKhauService();
+
+        public static string ConnectionString = @"Data Source=DESKTOP-IKSFK82\SQLEXPRESS;Initial Catalog=QuanLySieuThi;Integrated Security=True;";
+        //ConnectionString = @"Data Source=DESKTOP-IKSFK82\SQLEXPRESS;Initial Catalog=QuanLyBanHangNhanh;Integrated Security=True;";
+
+
+    }
+}
